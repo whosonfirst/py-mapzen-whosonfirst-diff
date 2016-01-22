779aeeac490561f54fcfde327d3a2456371f50f3
thisisaaronland <aaron@mapzen.com>
2016-01-22 21:20:10 +0000

    new code to actually look for changes

---
+++ mapzen/whosonfirst/diff/__init__.py
@@ -0,0 +1,116 @@
+# https://pythonhosted.org/setuptools/setuptools.html#namespace-packages
+__import__('pkg_resources').declare_namespace(__name__)
+
+import subprocess
+import geojson
+import os
+import json
+import hashlib
+
+import mapzen.whosonfirst.utils
+
+class diff:
+
+    def __init__(self):
+
+        self.wof = "/usr/local/mapzen/whosonfirst-data"
+
+    def compare(self, id, steps=1):
+
+        data = os.path.join(self.wof, "data")
+        current = mapzen.whosonfirst.utils.load(data, id)
+
+        rel_path = mapzen.whosonfirst.utils.id2relpath(id)
+        target = "HEAD~%d:data/%s" % (steps, rel_path)
+
+        cmd = [ "git", "-C", self.wof, "show", target ]
+        out = subprocess.check_output(cmd)
+
+        previous = geojson.loads(out)
+
+        """
+
+        If maybe I had even the vaguest of ideas what this error message even
+        meant we might try using this but as it is... (20160122/thisisaaronland)
+
+        diff = json_delta.udiff(previous, current)
+
+        Traceback (most recent call previous):
+        File "./__init__.py", line 33, in <module>
+        d.compare(101736545)
+        File "./__init__.py", line 27, in compare
+        diff = json_delta.udiff(previous, current)
+        File "build/bdist.linux-x86_64/egg/json_delta/_udiff.py", line 92, in udiff
+        File "build/bdist.linux-x86_64/egg/json_delta/_diff.py", line 72, in diff
+        File "build/bdist.linux-x86_64/egg/json_delta/_diff.py", line 128, in needle_diff
+        File "build/bdist.linux-x86_64/egg/json_delta/_diff.py", line 299, in keyset_diff
+        File "build/bdist.linux-x86_64/egg/json_delta/_diff.py", line 269, in compute_keysets
+        AssertionError: {"bbox": [-73.97435119999994, 45.40965310900003, -73.47412679299993, 
+        """
+
+        changes = {}
+
+        changes['geom'] = self.compare_geom(previous, current)
+
+        #
+
+        previous_props = previous.get('properties', {})
+        current_props = current.get('properties', {})
+        changes['properties'] = self.compare_object(previous_props, current_props)
+
+        #
+
+        wof = {
+            'concordances': {},
+            'hierarchy': [],
+            'supersedes': [],
+            'superseded_by': []
+        }
+
+        for k, v in wof.items():
+
+            wof_k = "wof:%s" % k
+
+            previous_el = previous_props.get(wof_k, {})
+            current_el = current_props.get(wof_k, {})
+
+            changes[k] = self.compare_object(previous_el, current_el)
+
+        #
+
+        return changes
+
+    def compare_geom(self, left, right):
+
+        left_hash = mapzen.whosonfirst.utils.hash_geom(left)
+        right_hash = mapzen.whosonfirst.utils.hash_geom(right)
+
+        return left_hash != right_hash
+
+    def compare_object_itemized(self, left, right):
+        # please write me...
+        pass
+
+    def compare_object(self, left_obj, right_obj):
+
+        left_hash = self.hash_obj(left_obj)
+        right_hash = self.hash_obj(right_obj)
+
+        return left_hash != right_hash
+
+    # put this in utils? probably...
+
+    def hash_obj(self, obj):
+
+        str_obj = json.dumps(obj)
+
+        hash = hashlib.md5()
+        hash.update(str_obj)
+        return hash.hexdigest()
+
+if __name__ == '__main__':
+
+    import pprint
+
+    d = diff()
+    print pprint.pformat(d.compare(101736545))
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.diff.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
+from setuptools import setup, find_packages
+
+packages = find_packages()
+version = open("VERSION").read()
+desc = open("README.md").read()
+
+setup(
+    name='mapzen.whosonfirst.diff',
+    namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.diff'],
+    version=version,
+    description='Simple Python wrapper for Who\'s On First helper functions',
+    author='Mapzen',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-diff',
+    install_requires=[
+        'mapzen.whosonfirst.utils>=0.19',
+        'geojson',
+        ],
+    dependency_links=[
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.19',
+        ],
+    packages=packages,
+    scripts=[
+        ],
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-diff/releases/tag/' + version,
+    license='BSD')
